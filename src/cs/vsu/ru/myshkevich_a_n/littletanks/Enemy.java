d3dd1e8e71bc8a7144e2c9dea40f4efc70fd04dd
123abc1920 <149964895+123abc1920@users.noreply.github.com>
2024-11-22 13:02:20 +0300

    Update Enemy.java

--- src/cs/vsu/ru/myshkevich_a_n/littletanks/Enemy.java
+++ src/cs/vsu/ru/myshkevich_a_n/littletanks/Enemy.java
@@ -1,6 +1,10 @@
 package cs.vsu.ru.myshkevich_a_n.littletanks;
 
 public class Enemy extends Tank {
+    private char symbolUp='^';
+    private char symbolLeft='>';
+    private char symbolRight='<';
+    private char symbolDown='v';
 
     public Enemy(int row, int col, Target target) {
         this.setCol(col);
