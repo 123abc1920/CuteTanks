910158d72ee6b90972ef588b59b569fff5179034
123abc1920 <123abc1920@mail.ru>
2024-11-25 15:20:05 +0300

    add walls lifes

--- src/cs/vsu/ru/myshkevich_a_n/littletanks/Wall.java
+++ src/cs/vsu/ru/myshkevich_a_n/littletanks/Wall.java
@@ -1,22 +1,23 @@
 package cs.vsu.ru.myshkevich_a_n.littletanks;
 
 public class Wall extends Cell {
-	private int lifes = 3;
-
 	public Wall() {
 		this.setSymbol(Global.wallSymbol);
 		this.setAvailable(false);
+		this.setLifes(3);
 	}
 
 	@Override
 	public char getSymbol() {
-		String s = Integer.toString(this.lifes);
+		String s = Integer.toString(this.getLifes());
 		return s.charAt(0);
 	}
 
 	@Override
 	public boolean setDestroy(Core core) {
+		System.out.println(this.getCol() + " " + this.getRow() + " " + this.getLifes());
 		this.setLifes(this.getLifes() - 1);
+		System.out.println(this.getCol() + " " + this.getRow() + " " + this.getLifes());
 		core.setNotAvailable();
 		this.setCore(null);
 		return false;
